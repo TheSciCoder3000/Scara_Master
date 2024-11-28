dbcbaba6f13d7556490eb59dc9180db5b5f6cf3d
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2024-11-28 09:38:23 +0800

    pin reassignment and commenting of some functions
    Co-authored-by: Joshua Chan <ThermalViz@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Wire_Master.h>
 #include "Arduino.h"
 
-#define LM1 17
-#define LM2 16
+#define LM1 5
+#define LM2 17
 #define LM3 33
 #define LM4 32
 
-#define BUT1 19
-#define BUT2 18
-#define BUT3 5
+#define BUT1 16
+#define BUT2 4
+#define BUT3 15
 
 // #define X2 34
 // #define X1 27
@@ -119,15 +119,17 @@ void loop()
       comm.transmit("STEP3CC1"); // ARM
     if (joystick2_dirX == 1)
       comm.transmit("STEP3CL1");
-    // if (joystick2_dirY == -1) comm.transmit("STEP4CC1");
-    // if (joystick2_dirY == 1) comm.transmit("STEP4CL1");
+    if (joystick2_dirY == -1)
+      comm.transmit("STEP4CC1");
+    if (joystick2_dirY == 1)
+      comm.transmit("STEP4CL1");
 
     // Serial.println("J1 X: " + String(joystick1_dirX) + "J1 Y: " + String(joystick1_dirY));
     // Serial.println("J1X: " + String(joystick1_X) + " | J1Y: " + String(joystick1_Y) + " | J2X: " + String(joystick2_X) + " | J2Y: " + String(joystick2_Y));
   }
 
   // checkLimits();
-  checkButtons();
+  // checkButtons();
 }
 
 void calibrate()
@@ -136,29 +138,29 @@ void calibrate()
 
 void checkLimits()
 {
-  if (digitalRead(LM1) == HIGH)
+  if (digitalRead(LM1) == LOW)
   {
     Serial.println("stopping 1: " + String(digitalRead(LM1)));
-    comm.transmit("STOP1");
+    // comm.transmit("STOP1");
   }
 
-  if (digitalRead(LM2) == HIGH)
-  {
-    Serial.println("stopping 2 " + String(digitalRead(LM2)));
-    comm.transmit("STOP2");
-  }
+  // if (digitalRead(LM2) == HIGH)
+  // {
+  //   Serial.println("stopping 2 " + String(digitalRead(LM2)));
+  //   // comm.transmit("STOP2");
+  // }
 
-  if (digitalRead(LM3) == HIGH)
-  {
-    Serial.println("stopping 3");
-    comm.transmit("STOP3");
-  }
+  // if (digitalRead(LM3) == HIGH)
+  // {
+  //   Serial.println("stopping 3");
+  //   // comm.transmit("STOP3");
+  // }
 
-  if (digitalRead(LM4) == HIGH)
-  {
-    Serial.println("stopping 4");
-    comm.transmit("STOP4");
-  }
+  // if (digitalRead(LM4) == HIGH)
+  // {
+  //   Serial.println("stopping 4");
+  //   // comm.transmit("STOP4");
+  // }
 }
 
 void checkButtons()
